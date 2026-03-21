d4ae486b657f622eb9cf017249234ae0e73c5910
m25237-beep <aroe0725@gmail.com>
2026-03-21 18:53:02 +0900

    feat:角度を入れたらdutyに変換するやつを作った（まだ未完成）

--- stm32_servo/src/app.cpp
+++ stm32_servo/src/app.cpp
@@ -1,8 +1,20 @@
 #include "stm32_servo/app.hpp"
 
+#include "fdcan.h"
+// #include "drivers/stm32"
+#include "gn10_can/core/can_bus.hpp"
+#include "gn10_can/devices/servo_motor_server.hpp"
 #include "tim.h"
 
+// gn10_can::drivers::DriverSTM32FDCAN can_driver(&hfdcan1);
+// gn10_can::CANBus can_bus(can_driver);
 extern TIM_HandleTypeDef htim2;
+
+uint32_t duty_set(float duty)
+{
+    float val = (duty * 1.0f / 180.0f + 1.0f) * 65535 / 100;
+    return uint32_t(val);
+}
 void setup()
 {
     HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
