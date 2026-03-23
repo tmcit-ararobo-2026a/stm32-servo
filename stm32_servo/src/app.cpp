957366c60de614e4b4e24a5b7279fb28d75b7948
Kamagata0 <aroe0725@gmail.com>
2026-03-23 15:56:16 +0900

    feat:受け取って角度を直す

--- stm32_servo/src/app.cpp
+++ stm32_servo/src/app.cpp
@@ -8,24 +8,23 @@
 
 gn10_can::drivers::DriverSTM32FDCAN can_driver(&hfdcan1);
 gn10_can::CANBus can_bus(can_driver);
-extern TIM_HandleTypeDef htim2;
-
-uint32_t duty_set(float duty)
-{
-    float val = (duty * 1.0f / 180.0f + 1.0f) * 65535 / 100;
-    return uint32_t(val);
-}
+gn10_can::devices::ServoMotorServer servo(can_bus, 0);
+uint32_t duty_set = 0;
 void setup()
 {
+    can_driver.init();
+    uint16_t new_min, new_max;
+    if (servo.get_new_init(new_min, new_max)) {
+    }
     HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
 }
 
 void loop()
 {
-    int x = 0;
-    x += 20;
-    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, x);
-    HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
-}
-extern "C" {
+    float angle_rad = 0;
+    can_bus.update();
+    if (servo.get_new_angle_rad(angle_rad)) {
+        duty_set = (uint32_t)((angle_rad * 9.5f / 180.0f + 2.5f) * 65535.0f / 100.0f);
+    }
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, duty_set);
 }
