1c460f8cc24c91a347dbe6ef8509835062afed71
m25237-beep <aroe0725@gmail.com>
2026-03-25 15:04:40 +0900

    chore:最後にdutyに変換するようにした

--- stm32_servo/src/app.cpp
+++ stm32_servo/src/app.cpp
@@ -9,15 +9,14 @@
 gn10_can::drivers::DriverSTM32FDCAN can_driver(&hfdcan1);
 gn10_can::CANBus can_bus(can_driver);
 gn10_can::devices::ServoMotorServer servo(can_bus, 0);
-uint32_t duty_set = 0;
+uint32_t duty_ms = 0;
 uint16_t max_us, min_us = 0;
-float pulse_width = 0;
+float pulse_width_ms     = 0;
+constexpr float MAX_DUTY = 19999.0f;
 void setup()
 {
     can_driver.init();
     uint16_t new_min, new_max;
-    if (servo.get_new_init(new_min, new_max)) {
-    }
     HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
 }
 
@@ -26,13 +25,14 @@ void loop()
     float angle_rad = 0;
     uint16_t new_max, new_min;
     if (servo.get_new_init(new_min, new_max)) {
-        max_us      = new_max;
-        min_us      = new_min;
-        pulse_width = (max_us - min_us) * 1000.0f;
+        max_us         = new_max;
+        min_us         = new_min;
+        pulse_width_ms = (max_us - min_us) * 1000.0f;
     }
     can_bus.update();
     if (servo.get_new_angle_rad(angle_rad)) {
-        duty_set = (uint32_t)(angle_rad * (pulse_width / 3.14159265359f) + min_us * 1000.0f);
+        duty_ms = (uint32_t)(angle_rad * (pulse_width_ms / 3.14159265359f) + min_us * 1000.0f);
     }
-    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, duty_set);
+    uint32_t duty = uint32_t(duty_ms * MAX_DUTY / 1000.0f);
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, duty_ms);
 }
