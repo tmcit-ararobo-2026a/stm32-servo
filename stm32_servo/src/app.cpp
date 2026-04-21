680540ab889d9ca3faf54fba4c3fa0a21806d5e4
kanai0914 <ayuayugonbe@gmail.com>
2026-04-21 22:26:36 +0900

    feat:最小構成でappを作成

--- stm32_servo/src/app.cpp
+++ stm32_servo/src/app.cpp
@@ -1,5 +1,7 @@
 #include "stm32_servo/app.hpp"
 
+#include <cmath>
+
 #include "drivers/stm32_fdcan/driver_stm32_fdcan.hpp"
 #include "fdcan.h"
 #include "gn10_can/core/can_bus.hpp"
@@ -8,32 +10,37 @@
 
 gn10_can::drivers::DriverSTM32FDCAN can_driver(&hfdcan1);
 gn10_can::CANBus can_bus(can_driver);
-gn10_can::devices::ServoMotorServer servo(can_bus, 0);
-float duty_ms = 0;
-uint16_t max_us, min_us = 0;
-float pulse_width_ms     = 0;
-constexpr float MAX_DUTY = 19999.0f;
+
+gn10_can::devices::ServoMotorServer servo0(can_bus, 0);
+
+uint16_t new_min, new_max;
+float angle_rad;
+
 void setup()
 {
     can_driver.init();
-    uint16_t new_min, new_max;
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+    servo0.get_new_init(new_min, new_max);
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
 }
 
 void loop()
 {
-    float angle_rad = 0;
-    uint16_t new_max, new_min;
-    if (servo.get_new_init(new_min, new_max)) {
-        max_us         = new_max;
-        min_us         = new_min;
-        pulse_width_ms = float(max_us - min_us) / 1000.0f;
+    if (servo0.get_new_init(new_min, new_max)) {
     }
+    if (servo0.get_new_angle_rad(angle_rad)) {
+        float pulse_f  = angle_rad / M_PI;
+        uint16_t pulse = new_min + pulse_f * (new_max - new_min);
+        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse);
+    }
+}
+
+extern "C" {
+// C言語側の関数のオーバーライド
+/**
+ * @brief Receive callback for FDCAN FIFO0.
+ */
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
+{
     can_bus.update();
-    if (servo.get_new_angle_rad(angle_rad)) {
-        duty_ms =
-            (angle_rad / 3.14159265359f) * pulse_width_ms + ((float)min_us / 1000.0f);
-        uint32_t duty = uint32_t((duty_ms / 20.0f) * MAX_DUTY);
-        __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, duty);
-    }
+}
 }
