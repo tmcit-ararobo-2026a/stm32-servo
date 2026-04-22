1a1a69c1d2f0ae7157724589186b17265d4d6c02
kanai0914 <ayuayugonbe@gmail.com>
2026-04-22 18:00:39 +0900

    TIM調整

--- stm32_servo/src/app.cpp
+++ stm32_servo/src/app.cpp
@@ -13,8 +13,9 @@ gn10_can::CANBus can_bus(can_driver);
 
 gn10_can::devices::ServoMotorServer servo0(can_bus, 0);
 
-uint16_t new_min, new_max;
 float angle_rad;
+uint16_t new_min = 1000;
+uint16_t new_max = 1350;
 
 void setup()
 {
@@ -32,15 +33,5 @@ void loop()
         uint16_t pulse = new_min + pulse_f * (new_max - new_min);
         __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse);
     }
-}
-
-extern "C" {
-// C言語側の関数のオーバーライド
-/**
- * @brief Receive callback for FDCAN FIFO0.
- */
-void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
-{
     can_bus.update();
-}
 }
